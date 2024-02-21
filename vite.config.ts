91deb170e553de59cdb499995a9c6652c2dd5c57
Caroline Desprat <cdesprat@icij.org>
2024-02-21 18:13:33 +0000

    chore: configure for Vue3 and boostrap vue next

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,59 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import vuePlugin from '@vitejs/plugin-vue2'
-import markdownPlugin from 'vite-plugin-md'
-import markdownItHighlightJs from 'markdown-it-highlightjs'
+import {fileURLToPath, URL} from "node:url";
+// generates d.ts files
+import DTS from "vite-plugin-dts";
+import Vue from '@vitejs/plugin-vue'
+import Delete from './plugins/plugin-delete'
+import VueDocgen from './plugins/plugin-docgen'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
-import markdownItApiTablePlugin from './plugins/markdown-it/api-table'
-import markdownItSampleCardPlugin from './plugins/markdown-it/sample-card'
-import docsPlugin from './plugins/docs'
-import frontMatterPlugin from './plugins/front-matter'
-import highlightPlugin from './plugins/highlight'
-import sassVarsPlugin from './plugins/sass-vars'
-import vueDocgenPlugin from './plugins/vue-docgen'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  test: {
-    globals: true,
-    reporters: 'basic',
-    environment: 'jsdom',
-    setupFiles: [resolve(__dirname, 'tests/unit/setup.js')]
-  },
+  assetsInclude: ['/sb-preview/**'],
+  plugins: [
+    Delete(),
+    Vue(),
+    VueDocgen(),
+    DTS({
+      exclude: [
+        'tests/**',
+        '**/*stories.ts',
+        'test',
+        'vite',
+        'vite.config.ts',
+        'vitest.config.ts',
+      ],
+      outDir: 'dist/lib',
+    }),
+    VueI18nPlugin({
+      /* options */
+      // locale messages resource pre-compile option
+      include: resolve(__dirname, 'lib/locales')
+    }),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),],
   build: {
     target: 'es2015',
     copyPublicDir: false,
+    outDir: 'dist/lib',
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'lib/main.js'),
       name: '@icij/murmur',
       fileName: 'lib/murmur'
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ['bootstrap','vue',{ 'bootstrap-vue-next': 'bootstrap-vue-next' }],
       output: {
         globals: {
           vue: 'Vue'
         },
         exports: 'named',
+        preserveModules: true,
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === 'style.css') {
             return 'lib/murmur.css'
@@ -49,42 +67,17 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 9009
   },
-  plugins: [
-    vuePlugin({
-      include: [/\.vue$/, /\.md$/]
-    }),
-    markdownPlugin({
-      vueVersion: '2.7.14',
-      escapeCodeTagInterpolation: false,
-      markdownItOptions: {
-        html: true,
-        xhtmlOut: false,
-        linkify: false,
-        typographer: false
-      },
-      markdownItSetup(md) {
-        // @ts-ignore
-        md.use(...markdownItApiTablePlugin())
-        // @ts-ignore
-        md.use(...markdownItSampleCardPlugin())
-        md.use(markdownItHighlightJs)
-      }
-    }),
-    docsPlugin(),
-    highlightPlugin(),
-    frontMatterPlugin(),
-    sassVarsPlugin(),
-    vueDocgenPlugin()
-  ],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
+      vue: resolve(__dirname, './node_modules/vue'),
       node_modules: resolve(__dirname, 'node_modules'),
       $package: resolve(__dirname, 'package.json'),
-      '@': resolve(__dirname, 'lib'),
+      '@': fileURLToPath(new URL('./lib', import.meta.url)),
       $docs: resolve(__dirname, 'docs'),
       $components: resolve(__dirname, 'docs/components'),
       $pages: resolve(__dirname, 'docs/pages')
-    }
+    },
+    dedupe: ['vue']
   }
 })
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'node:url'
+
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            globals: true,
+            clearMocks: false,
+            setupFiles: ['tests/unit/setup.js'],
+            reporters: 'basic',
+            environment: 'jsdom',
+            exclude: [...configDefaults.exclude, 'e2e/*'],
+            root: fileURLToPath(new URL('./', import.meta.url))
+        }
+    })
+)
