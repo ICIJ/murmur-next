83f7a75f0db783c2b7ea6b8fe3ecb9a6bc4d5583
Caroline Desprat <cdesprat@icij.org>
2024-02-21 18:13:33 +0000

    chore: configure for Vue3 and boostrap vue next

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
+import { config } from '@vue/test-utils';
+import {i18n} from "../../lib/i18n";
 
-Vue.config.productionTip = false
-Vue.config.devtools = false
-
+// @ts-expect-error type
+  config.global.plugins = [i18n];
+  // @ts-expect-error type
 Object.defineProperty(document, 'fonts', {
   value: { ready: Promise.resolve({}) },
 })
+global.HTMLCanvasElement.prototype.getContext = () => null;
+global.HTMLCanvasElement.prototype.toDataURL = () => null;
+global.URL.createObjectURL = () => "";
