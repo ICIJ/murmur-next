313b0b865e86fac5f1e040fe1bc17b8df8d10c00
Caroline Desprat <cdesprat@icij.org>
2024-02-23 10:47:53 +0000

    chore: add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,34 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+import { mergeConfig } from "vite";
+
+const config:StorybookConfig = {
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/preset-scss"
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: (config, options) => {
+    return mergeConfig(config, {
+      // This is needed because the copy plugin can't copy files outside of the root directory
+      // The files are copied back to the root by the post script
+      build: {
+        outDir: 'storybook-static/dist'
+      }
+    });
+  }
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+import {setup, Preview} from '@storybook/vue3'
+import Murmur from "@/main";
+setup((app)=>{
+  app.use(Murmur)
+})
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+const preview: Preview = {
+  parameters
+};
+export default preview;
