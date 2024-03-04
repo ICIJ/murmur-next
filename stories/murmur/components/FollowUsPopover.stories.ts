0005868c80838fa9af2fa57d562c60843be15cf4
Caroline Desprat <cdesprat@icij.org>
2024-03-04 18:30:58 +0000

    storybook: add FollowUsPopover stories

---
+++ stories/murmur/components/FollowUsPopover.stories.ts
@@ -0,0 +1,24 @@
+import { FollowUsPopover } from "@/components";
+import { StoryObj } from "@storybook/vue3";
+
+export default {
+    title: 'Murmur/components/FollowUsPopover',
+    component: FollowUsPopover,
+    tags: ['autodocs'],
+    argTypes: {
+    }
+};
+
+type Story = StoryObj<typeof FollowUsPopover>;
+const Template: Story = (args: any) => ({
+    components: { FollowUsPopover },
+    setup() {
+        return { args };
+    },
+    template: '<FollowUsPopover v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    class:"card card-xs my-4 mx-auto border border-primary"
+};
