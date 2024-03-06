f4456037f29dfc4d9a61a71d33d197254dbeba1b
Caroline Desprat <cdesprat@icij.org>
2024-03-06 13:17:48 +0000

    storybook: add GenericHeader stories

---
+++ stories/murmur/components/GenericHeader.stories.ts
@@ -0,0 +1,26 @@
+import { GenericHeader } from "@/components";
+import { StoryObj } from "@storybook/vue3";
+
+export default {
+    title: 'Murmur/components/GenericHeader',
+    component: GenericHeader,
+    tags: ['autodocs'],
+    argTypes: {
+        brandOptions:{type:"select",}
+    }
+};
+
+type Story = StoryObj<typeof GenericHeader>;
+const Template: Story = (args: any) => ({
+    components: { GenericHeader },
+    setup() {
+        return { args };
+    },
+    template: '<GenericHeader v-bind="args" ></GenericHeader>',
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+Default.parameters = { layout: 'fullscreen' }
+export const NoHeadroom = Template.bind({});
+NoHeadroom.args = {noHeadroom:true, position:"relative"};
