2ad02009467a7ef5a54a511c06147b25cdaed380
Caroline Desprat <cdesprat@icij.org>
2024-03-07 17:17:55 +0000

    fix: update tests

--- tests/unit/components/FollowUsPopover.spec.js
+++ tests/unit/components/FollowUsPopover.spec.js
@@ -21,9 +21,9 @@ describe('FollowUsPopover.vue', () => {
   it('emits an event when clicking on close button', async () => {
     const wrapper = shallowMount(FollowUsPopover)
     const btn = wrapper.find('.follow-us__close')
-    expect(wrapper.emitted('update:show')).toBeFalsy()
+    expect(wrapper.emitted('update:close')).toBeFalsy()
     await btn.trigger('click')
-    expect(wrapper.emitted('update:show')).toBeTruthy()
+    expect(wrapper.emitted('update:close')).toBeTruthy()
   })
 
 })
