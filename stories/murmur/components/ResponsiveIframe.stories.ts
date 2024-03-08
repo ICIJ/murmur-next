23188bb393892dfe00ea3b79fcc77607be84adb5
Caroline Desprat <cdesprat@icij.org>
2024-03-08 18:01:41 +0000

    storybook: add ResponsiveIframe stories

---
+++ stories/murmur/components/ResponsiveIframe.stories.ts
@@ -0,0 +1,24 @@
+import { ResponsiveIframe } from "@/components";
+import { StoryObj } from "@storybook/vue3";
+
+export default {
+    title: 'Murmur/components/ResponsiveIframe',
+    component: ResponsiveIframe,
+    tags: ['autodocs'],
+    argTypes: {
+    }
+};
+
+type Story = StoryObj<typeof ResponsiveIframe>;
+const Template: Story = (args: any) => ({
+    components: { ResponsiveIframe },
+    setup() {
+        return { args };
+    },
+    template: `<ResponsiveIframe v-bind="args" />`,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    url:"https://projects.icij.org/the-implant-files/graphics/#/adverse-events?no-embeddable-footer=1"
+};
