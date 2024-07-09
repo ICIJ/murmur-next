49a0ab40a07d44d8c705a627ad3c63f1ac9cb1fc
Pierre Romera <hello@pirhoo.com>
2024-07-09 14:32:52 +0000

    feat: add first/last nav

--- lib/components/TinyPagination.vue
+++ lib/components/TinyPagination.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="tiny-pagination" :class="paginationClassList">
     <b-button
+      class="tiny-pagination__nav tiny-pagination__nav--bound  tiny-pagination__nav--first"
+      :size="size"
+      :disabled="!hasFirst"
+      :variant="navVariant"
+      @click="applyFirstPage"
+    >
+      <!-- @slot Previous button content -->
+      <slot
+        name="first"
+        v-bind="{ modelValue, numberOfPages, hasFirst, hasPrevious, hasNext, hasLast }"
+      >
+        <phosphor-icon :name="firstPageIcon" />
+        <span class="visually-hidden">{{ t('tiny-pagination.first') }}</span>
+      </slot>
+    </b-button>
+    <b-button
       class="tiny-pagination__nav"
       :size="size"
       :disabled="!hasPrevious"
@@ -57,11 +73,27 @@
         <span class="visually-hidden">{{ t('tiny-pagination.next') }}</span>
       </slot>
     </b-button>
+    <b-button
+      class="tiny-pagination__nav tiny-pagination__nav--bound  tiny-pagination__nav--last"
+      :size="size"
+      :disabled="!hasLast"
+      :variant="navVariant"
+      @click="applyLastPage"
+    >
+      <!-- @slot Previous button content -->
+      <slot
+        name="last"
+        v-bind="{ modelValue, numberOfPages, hasFirst, hasPrevious, hasNext, hasLast }"
+      >
+        <phosphor-icon :name="lastPageIcon" />
+        <span class="visually-hidden">{{ t('tiny-pagination.last') }}</span>
+      </slot>
+    </b-button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType, ref, computed, watch } from 'vue'
+<script lang="ts" setup>
+import { PropType, ref, computed, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 import { Size } from '@/enums'
@@ -69,150 +101,161 @@ import { ButtonVariant, BFormInput, BButton } from 'bootstrap-vue-next'
 
 import PhosphorIcon from './PhosphorIcon.vue'
 
-export default defineComponent({
-  name: 'TinyPagination',
-  components: {
-    PhosphorIcon,
-    BFormInput,
-    BButton
+
+const props = defineProps({
+  /**
+   * Total items to be stored in pages
+   */
+  totalRows: {
+    type: Number,
+    default: 0
   },
-  props: {
-    /**
-     * Total items to be stored in pages
-     */
-    totalRows: {
-      type: Number,
-      default: 0
-    },
-    /**
-     * Sets the quantity of items per page
-     */
-    perPage: {
-      type: Number,
-      default: 20
-    },
-    /**
-     * Grabs and syncs the currentPage variable passed down from the parent in v-model
-     */
-    modelValue: {
-      type: [Number, String],
-      default: 1
-    },
-    /**
-     * Set the size of the input: 'sm', 'md' (default), or 'lg'.
-     */
-    size: {
-      type: String as PropType<Size>,
-      default: Size.md,
-      validator: (value: Size) => Object.values(Size).includes(value)
-    },
-    /**
-     * (Optional) Number of page. Property `size` is required for this to work
-     * properly. If `pages` is empty, it will be calculated using the size.
-     */
-    pages: {
-      type: [Number, String],
-      default: null
-    },
-    /**
-     * Hide navigation buttons (next and previous)
-     */
-    noNav: {
-      type: Boolean
-    },
-    /**
-     * FontAwesome icon of the previous page button
-     */
-    previousPageIcon: {
-      type: String,
-      default: 'caret-left'
-    },
-    /**
-     * FontAwesome icon of the next page button
-     */
-    nextPageIcon: {
-      type: String,
-      default: 'caret-right'
-    },
-    /**
-     * Navigation button variants
-     */
-    navVariant: {
-      type: String as PropType<ButtonVariant>,
-      default: 'link'
-    },
-    /**
-     * Display pagination as a block (full width)
-     */
-    block: {
-      type: Boolean
-    },
-    /**
-     * Compact mode with a grouped nav
-     */
-    compact: {
-      type: Boolean
-    }
+  /**
+   * Sets the quantity of items per page
+   */
+  perPage: {
+    type: Number,
+    default: 20
   },
-  emits: ['update:modelValue'],
-  setup(props, { emit }) {
-    const { t } = useI18n()
-
-    const numberOfPages = computed((): number => {
-      if (props.pages === null) {
-        return Math.ceil(props.totalRows / props.perPage)
-      }
-      return Number(props.pages)
-    })
-
-    const paginationClassList = computed((): object => {
-      return {
-        [`tiny-pagination--${props.size}`]: true,
-        [`tiny-pagination--no-nav`]: props.noNav,
-        [`tiny-pagination--block`]: props.block,
-        [`tiny-pagination--compact`]: props.compact
-      }
-    })
-
-    const hasPrevious = computed((): boolean => pageValue.value > 1)
-    const hasNext = computed(
-      (): boolean => pageValue.value < numberOfPages.value
-    )
-    const pageValue = computed(() => +props.modelValue)
-
-    const currentPageInput = ref<number | string>(pageValue.value)
-
-    watch(() => props.modelValue, (value) => {
-      currentPageInput.value = value
-    })
-
-    function applyPageForm(): void {
-      if (!isNaN(currentPageInput.value as number)) {
-        emit('update:modelValue', +currentPageInput.value)
-      }
-    }
-
-    function applyNextPage(): void {
-      emit('update:modelValue', pageValue.value + 1)
-    }
-
-    function applyPreviousPage(): void {
-      emit('update:modelValue', pageValue.value - 1)
-    }
-
-    return {
-      t,
-      currentPageInput,
-      paginationClassList,
-      numberOfPages,
-      hasPrevious,
-      hasNext,
-      applyPreviousPage,
-      applyNextPage,
-      applyPageForm
-    }
+  /**
+   * Grabs and syncs the currentPage variable passed down from the parent in v-model
+   */
+  modelValue: {
+    type: [Number, String],
+    default: 1
+  },
+  /**
+   * Set the size of the input: 'sm', 'md' (default), or 'lg'.
+   */
+  size: {
+    type: String as PropType<Size>,
+    default: Size.md,
+    validator: (value: Size) => Object.values(Size).includes(value)
+  },
+  /**
+   * (Optional) Number of page. Property `size` is required for this to work
+   * properly. If `pages` is empty, it will be calculated using the size.
+   */
+  pages: {
+    type: [Number, String],
+    default: null
+  },
+  /**
+   * Hide navigation buttons (next and previous)
+   */
+  noNav: {
+    type: Boolean
+  },
+  /**
+   * Hide bounds navigation buttons (first and last)
+   */
+  noNavBounds: {
+    type: Boolean
+  },
+  /**
+   * Phosphor icon of the previous page button
+   */
+  previousPageIcon: {
+    type: String,
+    default: 'caret-left'
+  },
+  /**
+   * Phosphor icon of the next page button
+   */
+  nextPageIcon: {
+    type: String,
+    default: 'caret-right'
+  },
+  /**
+   * Phosphor icon of the first page button
+   */
+  firstPageIcon: {
+    type: String,
+    default: 'caret-double-left'
+  },
+  /**
+   * Phosphor icon of the last page button
+   */
+  lastPageIcon: {
+    type: String,
+    default: 'caret-double-right'
+  },
+  /**
+   * Navigation button variants
+   */
+  navVariant: {
+    type: String as PropType<ButtonVariant>,
+    default: 'link'
+  },
+  /**
+   * Display pagination as a block (full width)
+   */
+  block: {
+    type: Boolean
+  },
+  /**
+   * Compact mode with a grouped nav
+   */
+  compact: {
+    type: Boolean
   }
 })
+
+const emit = defineEmits(['update:modelValue'])
+const { t } = useI18n()
+
+const numberOfPages = computed((): number => {
+  if (props.pages === null) {
+    return Math.ceil(props.totalRows / props.perPage)
+  }
+  return Number(props.pages)
+})
+
+const paginationClassList = computed((): object => {
+  return {
+    [`tiny-pagination--${props.size}`]: true,
+    [`tiny-pagination--no-nav`]: props.noNav,
+    [`tiny-pagination--no-nav-bounds`]: props.noNavBounds,
+    [`tiny-pagination--block`]: props.block,
+    [`tiny-pagination--compact`]: props.compact
+  }
+})
+
+const hasFirst = computed((): boolean => pageValue.value > 1)
+const hasPrevious = computed((): boolean => hasFirst.value)
+
+const hasLast = computed((): boolean => pageValue.value < numberOfPages.value)
+const hasNext = computed((): boolean => hasLast.value)
+
+const pageValue = computed(() => +props.modelValue)
+
+const currentPageInput = ref<number | string>(pageValue.value)
+
+watch(() => props.modelValue, (value) => {
+  currentPageInput.value = value
+})
+
+function applyPageForm(): void {
+  if (!isNaN(currentPageInput.value as number)) {
+    emit('update:modelValue', +currentPageInput.value)
+  }
+}
+
+function applyNextPage(): void {
+  emit('update:modelValue', pageValue.value + 1)
+}
+
+function applyPreviousPage(): void {
+  emit('update:modelValue', pageValue.value - 1)
+}
+
+function applyFirstPage(): void {
+  emit('update:modelValue', 1)
+}
+
+function applyLastPage(): void {
+  emit('update:modelValue', numberOfPages.value)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -223,6 +266,16 @@ export default defineComponent({
   align-items: center;
   justify-content: center;
   text-align: center;
+
+  &__nav {
+    padding-left: 0.25em;
+    padding-right: 0.25em;
+    color: var(--bs-body-color);
+
+    &:hover {
+      color: var(--bs-link-hover-color);
+    }
+  }
 
   &--block {
     display: flex;
@@ -237,6 +290,11 @@ export default defineComponent({
   }
 
   &--no-nav &__nav {
+    display: none;
+  }
+
+  &--compact &__nav--bound,
+  &--no-nav-bounds &__nav--bound {
     display: none;
   }
 
