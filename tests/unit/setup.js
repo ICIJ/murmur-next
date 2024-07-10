f773504917190be22730020679908c125c031fdc
Pierre Romera <hello@pirhoo.com>
2024-07-10 09:16:19 +0000

    test: mock resize observer

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -4,10 +4,18 @@ import createBootstrap from 'bootstrap-vue-next'
 
 // @ts-expect-error type
 config.global.plugins = [i18n, createBootstrap()]
+
 // @ts-expect-error type
 Object.defineProperty(document, 'fonts', {
   value: { ready: Promise.resolve({}) }
 })
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn()
+}))
+
 global.HTMLCanvasElement.prototype.getContext = () => null
 global.HTMLCanvasElement.prototype.toDataURL = () => null
 global.URL.createObjectURL = () => ''
