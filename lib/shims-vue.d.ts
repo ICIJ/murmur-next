12e97c6d53124e81d3eb7aa9b3611b996a507fbf
Caroline Desprat <cdesprat@icij.org>
2025-02-05 11:30:57 +0000

    revert: remove module declaration

--- lib/shims-vue.d.ts
+++ lib/shims-vue.d.ts
@@ -12,4 +12,3 @@ declare module '*.vue' {
   const component: DefineComponent<NonNullable<unknown>, NonNullable<unknown>, any>
   export default component
 }
-declare module 'bootstrap-vue-next/src/utils/floatingUi';
