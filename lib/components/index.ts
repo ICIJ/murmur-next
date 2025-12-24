f5df84337c545e6749ae2e5f241acc3f9096e657
Pierre Romera <hello@pirhoo.com>
2025-12-24 18:07:21 +0000

    feat(exports): add deprecated aliases for backward compatibility
    Add @deprecated JSDoc annotations and export aliases for all renamed
    components to maintain backward compatibility:
    - ConfirmButton → ButtonConfirm
    - CustomPagination → Pagination
    - TinyPagination → PaginationTiny
    - OrdinalLegend → LegendOrdinal
    - ScaleLegend → LegendScale
    - Form components with old naming conventions
    
    This allows existing code to continue working while migrating to the
    new naming conventions.

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,36 +1,101 @@
+// Accordion
 export { default as AccordionWrapper } from './Accordion/AccordionWrapper.vue'
 export { default as AccordionStep } from './Accordion/AccordionStep.vue'
+
+// ActiveTextTruncate
 export { default as ActiveTextTruncate } from './ActiveTextTruncate/ActiveTextTruncate.vue'
-export { default as AdvancedLinkForm } from './AdvancedLinkForm/AdvancedLinkForm.vue'
-export { default as AdvancedLinkFormTab } from './AdvancedLinkForm/AdvancedLinkFormTab.vue'
+
+// Brand
 export { default as Brand } from './Brand/Brand.vue'
 export { default as BrandExpansion } from './Brand/BrandExpansion.vue'
+
+// Button
 export { default as ButtonIcon } from './Button/ButtonIcon.vue'
 export { default as ButtonIconCounter } from './Button/ButtonIconCounter.vue'
-export { default as ConfirmButton } from './Button/ConfirmButton.vue'
+export { default as ButtonConfirm } from './Button/ButtonConfirm.vue'
+/** @deprecated Use ButtonConfirm instead */
+export { default as ConfirmButton } from './Button/ButtonConfirm.vue'
+
+// ContentPlaceholder
 export { default as ContentPlaceholder } from './ContentPlaceholder/ContentPlaceholder.vue'
-export { default as CustomPagination } from './Pagination/CustomPagination.vue'
-export { default as DigitsInput } from './DigitsInput/DigitsInput.vue'
-export { default as DonateForm } from './DonateForm/DonateForm.vue'
+
+// EmbeddableFooter
 export { default as EmbeddableFooter } from './EmbeddableFooter/EmbeddableFooter.vue'
-export { default as EmbedForm } from './EmbedForm/EmbedForm.vue'
+
+// FollowUsPopover
 export { default as FollowUsPopover } from './FollowUsPopover/FollowUsPopover.vue'
+
+// Form
+export { default as FormAdvancedLink } from './Form/FormAdvancedLink/FormAdvancedLink.vue'
+/** @deprecated Use FormAdvancedLink instead */
+export { default as AdvancedLinkForm } from './Form/FormAdvancedLink/FormAdvancedLink.vue'
+export { default as FormAdvancedLinkTab } from './Form/FormAdvancedLink/FormAdvancedLinkTab.vue'
+/** @deprecated Use FormAdvancedLinkTab instead */
+export { default as AdvancedLinkFormTab } from './Form/FormAdvancedLink/FormAdvancedLinkTab.vue'
+export { default as FormDonate } from './Form/FormDonate.vue'
+/** @deprecated Use FormDonate instead */
+export { default as DonateForm } from './Form/FormDonate.vue'
+export { default as FormEmbed } from './Form/FormEmbed.vue'
+/** @deprecated Use FormEmbed instead */
+export { default as EmbedForm } from './Form/FormEmbed.vue'
+export { default as FormSignUp } from './Form/FormSignUp.vue'
+/** @deprecated Use FormSignUp instead */
+export { default as SignUpForm } from './Form/FormSignUp.vue'
+
+// Form/FormControl
+export { default as FormControlDigits } from './Form/FormControl/FormControlDigits.vue'
+/** @deprecated Use FormControlDigits instead */
+export { default as DigitsInput } from './Form/FormControl/FormControlDigits.vue'
+export { default as FormControlRange } from './Form/FormControl/FormControlRange.vue'
+/** @deprecated Use FormControlRange instead */
+export { default as RangePicker } from './Form/FormControl/FormControlRange.vue'
+export { default as FormControlSecret } from './Form/FormControl/FormControlSecret.vue'
+/** @deprecated Use FormControlSecret instead */
+export { default as SecretInput } from './Form/FormControl/FormControlSecret.vue'
+export { default as FormControlSelectableDropdown } from './Form/FormControl/FormControlSelectableDropdown.vue'
+/** @deprecated Use FormControlSelectableDropdown instead */
+export { default as SelectableDropdown } from './Form/FormControl/FormControlSelectableDropdown.vue'
+
+// Generic
 export { default as GenericFooter } from './Generic/GenericFooter.vue'
 export { default as GenericHeader } from './Generic/GenericHeader.vue'
+
+// HapticCopy
 export { default as HapticCopy } from './HapticCopy/HapticCopy.vue'
+
+// ImageMode
 export { default as ImageMode } from './ImageMode/ImageMode.vue'
 export { default as ImageModeSource } from './ImageMode/ImageModeSource.vue'
-export { default as OrdinalLegend } from './Legend/OrdinalLegend.vue'
+
+// Legend
+export { default as LegendOrdinal } from './Legend/LegendOrdinal.vue'
+/** @deprecated Use LegendOrdinal instead */
+export { default as OrdinalLegend } from './Legend/LegendOrdinal.vue'
+export { default as LegendScale } from './Legend/LegendScale.vue'
+/** @deprecated Use LegendScale instead */
+export { default as ScaleLegend } from './Legend/LegendScale.vue'
+
+// Pagination
+export { default as Pagination } from './Pagination/Pagination.vue'
+/** @deprecated Use Pagination instead */
+export { default as CustomPagination } from './Pagination/Pagination.vue'
+export { default as PaginationTiny } from './Pagination/PaginationTiny.vue'
+/** @deprecated Use PaginationTiny instead */
+export { default as TinyPagination } from './Pagination/PaginationTiny.vue'
+
+// PhosphorIcon
 export { default as PhosphorIcon } from './PhosphorIcon/PhosphorIcon.vue'
 export { default as PhosphorIconLayers } from './PhosphorIcon/PhosphorIconLayers.vue'
-export { default as RangePicker } from './RangePicker/RangePicker.vue'
+
+// ResponsiveIframe
 export { default as ResponsiveIframe } from './ResponsiveIframe/ResponsiveIframe.vue'
-export { default as ScaleLegend } from './Legend/ScaleLegend.vue'
-export { default as SecretInput } from './SecretInput/SecretInput.vue'
-export { default as SelectableDropdown } from './SelectableDropdown/SelectableDropdown.vue'
+
+// SharingOptions
 export { default as SharingOptions } from './SharingOptions/SharingOptions.vue'
 export { default as SharingOptionsLink } from './SharingOptions/SharingOptionsLink.vue'
-export { default as SignUpForm } from './SignUpForm/SignUpForm.vue'
+
+// SlideUpDown
 export { default as SlideUpDown } from './SlideUpDown/SlideUpDown.vue'
+
+// TexturedDeck
 export { default as TexturedDeck } from './TexturedDeck/TexturedDeck.vue'
-export { default as TinyPagination } from './Pagination/TinyPagination.vue'
