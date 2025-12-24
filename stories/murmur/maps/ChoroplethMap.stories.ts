d1c0834cec8249121ad7012bd58cbd441c80e385
Pierre Romera <hello@pirhoo.com>
2025-12-24 13:09:54 +0000

    docs(ChoroplethMap): add comprehensive stories

--- stories/murmur/maps/ChoroplethMap.stories.ts
+++ stories/murmur/maps/ChoroplethMap.stories.ts
@@ -3,7 +3,7 @@ import { StoryObj } from '@storybook/vue3-vite'
 import { scaleThreshold } from 'd3'
 import { geoOrthographic } from 'd3-geo'
 
-const motorVehiclesPer1000people = {
+const motorVehiclesPer1000people: Record<string, number> = {
   MCO: 899,
   USA: 797,
   LIE: 750,
@@ -195,7 +195,8 @@ const motorVehiclesPer1000people = {
   SOM: 3,
   TGO: 2
 }
-const wineStockByDepartment = {
+
+const wineStockByDepartment: Record<string, number> = {
   '01': 10155,
   '02': 686,
   '03': 851,
@@ -293,159 +294,288 @@ const wineStockByDepartment = {
   '94': 13394,
   '95': 840
 }
+
 function featureColorScale() {
-  return scaleThreshold()
+  return scaleThreshold<number, string>()
     .domain([100, 300, 700])
     .range(['#ffffcc', '#c2e699', '#78c679', '#238443'])
 }
+
 export default {
   title: 'Murmur/maps/ChoroplethMap',
   component: ChoroplethMap,
   tags: ['autodocs'],
-  argTypes: {}
+  argTypes: {
+    data: { control: 'object' },
+    hatchEmpty: { control: 'boolean' },
+    zoomable: { control: 'boolean' },
+    clickable: { control: 'boolean' },
+    hideLegend: { control: 'boolean' }
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `
+The \`ChoroplethMap\` component creates thematic maps where areas are shaded based on data values.
+
+## Data Structure
+
+Pass an object with region identifiers as keys (ISO3 codes for world maps) and numeric values:
+
+\`\`\`javascript
+{
+  FRA: 578,  // France
+  DEU: 572,  // Germany
+  USA: 797,  // United States
+  // ...
+}
+\`\`\`
+
+## Custom TopoJSON
+
+You can use different TopoJSON files to create maps of specific regions (countries, departments, etc.)
+by configuring \`topojsonUrl\`, \`topojsonObjects\`, and \`topojsonObjectsPath\`.
+        `
+      }
+    }
+  }
 }
 
 type Story = StoryObj<typeof ChoroplethMap>
-const Template: Story = (args: any) => ({
-  components: { ChoroplethMap },
-  setup() {
-    return { args }
+
+export const Default: Story = {
+  name: 'Motor Vehicles per 1000 People',
+  args: {
+    data: motorVehiclesPer1000people,
+    hatchEmpty: true,
+    zoomable: true
   },
-  template: `<ChoroplethMap v-bind="args" />`
-})
-
-export const Default = Template.bind({})
-Default.args = {
-  data: motorVehiclesPer1000people
-}
-
-export const HatchEmpty = Template.bind({})
-
-HatchEmpty.args = {
-  data: motorVehiclesPer1000people,
-  hatchEmpty: true
-}
-export const Zoomable = Template.bind({})
-
-Zoomable.args = {
-  data: motorVehiclesPer1000people,
-  hatchEmpty: true,
-  zoomable: true
-}
-export const FeatureColorScale = Template.bind({})
-
-FeatureColorScale.args = {
-  data: motorVehiclesPer1000people,
-  featureColorScale: featureColorScale()
-}
-
-export const Topojson = Template.bind({})
-
-Topojson.args = {
-  data: wineStockByDepartment,
-  clickable: true,
-  zoomable: true,
-  topojsonUrl: './assets/topojson/france-departments.json',
-  topojsonObjects: 'departements',
-  topojsonObjectsPath: 'properties.code'
-}
-
-const TemplateLegend: Story = (args: any) => ({
-  components: { ChoroplethMap },
-  setup() {
-    return { args }
-  },
-  template: `
-    <choropleth-map v-bind="args">
-      <template #legend-cursor="{ value, identifier }">
-        <div class="bg-dark text-light px-2 py-1 text-nowrap">
-          <span>{{value.toLocaleString()}} hl</span>
-        </div>
-      </template>
- 
-    </choropleth-map>`
-})
-
-export const TopojsonLegend = TemplateLegend.bind({})
-
-TopojsonLegend.args = {
-  data: wineStockByDepartment,
-  clickable: true,
-  zoomable: true,
-  topojsonUrl: './assets/topojson/france-departments.json',
-  topojsonObjects: 'departements',
-  topojsonObjectsPath: 'properties.code'
-
-}
-const TemplateAnnotation: Story = (args: any) => ({
-  components: { ChoroplethMap, ChoroplethMapAnnotation },
-  setup() {
-    const annotation = {
-      latitude: 44.836151,
-      longitude: -0.580816,
-      placement: 'righttop',
-      class: 'text-center'
+  render: args => ({
+    components: { ChoroplethMap },
+    setup: () => ({ args }),
+    template: `
+      <div class="bg-light p-4">
+        <h4 class="mb-4">Motor vehicles per 1000 people</h4>
+        <ChoroplethMap v-bind="args" />
+        <p class="text-end mt-2 mb-0">
+          <small><a href="https://en.wikipedia.org/wiki/List_of_countries_by_vehicles_per_capita" target="_blank">Source: Wikipedia</a></small>
+        </p>
+      </div>
+    `
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: 'A world map showing motor vehicles per 1000 people by country. Uses ISO3 country codes as identifiers.'
+      }
     }
-    return { args, annotation }
-  },
-  template: `
-    <choropleth-map v-bind="args">
-      <template #legend-cursor="{ value, identifier }">
-        <div class="bg-dark text-light px-2 py-1 text-nowrap">
-          <span>{{value.toLocaleString()}} hl</span>
-        </div>
-      </template>
-      <choropleth-map-annotation v-bind="annotation">
-        Bordeaux<br /><img src="/assets/img/arrow-bottom.svg" width="16px" />
-      </choropleth-map-annotation>
-    </choropleth-map>`
-})
-
-export const TopojsonAnnotation = TemplateAnnotation.bind({})
-
-TopojsonAnnotation.args = {
-  data: wineStockByDepartment,
-  clickable: true,
-  zoomable: true,
-  topojsonUrl: './assets/topojson/france-departments.json',
-  topojsonObjects: 'departements',
-  topojsonObjectsPath: 'properties.code'
+  }
 }
 
-const TemplateCustomProjection: Story = (args: any) => ({
-  components: { ChoroplethMap, ChoroplethMapAnnotation },
-  setup() {
-    const annotation = {
-      class: 'text-center',
-      height: 15,
-      latitude: 35.167406,
-      longitude: 33.435499,
-      width: 15,
-      dropShadow: 'none',
-      scale: true
-    }
-    return { args, annotation }
+export const CustomColorScale: Story = {
+  name: 'Custom Color Scale',
+  args: {
+    data: motorVehiclesPer1000people,
+    featureColorScale: featureColorScale()
   },
-  template: `
-    <choropleth-map v-bind="args">
-      <choropleth-map-annotation v-bind="annotation">
-        <div class="border border-warning" style="height: 15px; width: 15px"></div>
-      </choropleth-map-annotation>
-    </choropleth-map>`
-})
+  render: args => ({
+    components: { ChoroplethMap },
+    setup: () => ({ args }),
+    template: `
+      <div class="bg-light p-4">
+        <h4 class="mb-4">Motor vehicles per 1000 people</h4>
+        <ChoroplethMap v-bind="args" />
+        <p class="text-end mt-2 mb-0">
+          <small><a href="https://en.wikipedia.org/wiki/List_of_countries_by_vehicles_per_capita" target="_blank">Source: Wikipedia</a></small>
+        </p>
+      </div>
+    `
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: `
+Use a custom D3 scale function to colorize the map features:
 
-export const CustomProjection = TemplateCustomProjection.bind({})
+\`\`\`javascript
+import { scaleThreshold } from 'd3'
 
-CustomProjection.args = {
-  center: [33.435499, 35.167406],
+const featureColorScale = scaleThreshold()
+  .domain([100, 300, 700])
+  .range(['#ffffcc', '#c2e699', '#78c679', '#238443'])
+\`\`\`
+        `
+      }
+    }
+  }
+}
+
+export const FrenchDepartments: Story = {
+  name: 'French Departments (Custom TopoJSON)',
+  args: {
+    data: wineStockByDepartment,
+    clickable: true,
+    zoomable: true,
+    topojsonUrl: './assets/topojson/france-departments.json',
+    topojsonObjects: 'departements',
+    topojsonObjectsPath: 'properties.code'
+  },
+  render: args => ({
+    components: { ChoroplethMap, ChoroplethMapAnnotation },
+    setup: () => ({ args }),
+    template: `
+      <div class="bg-light p-4">
+        <h4>Wine stocks (hectoliters) by French Department</h4>
+        <p>Commercial stocks of wine for each French department in August 2016.</p>
+        <ChoroplethMap v-bind="args">
+          <template #legend-cursor="{ value }">
+            <div class="bg-dark text-light px-2 py-1 text-nowrap">
+              {{ value.toLocaleString() }} hl
+            </div>
+          </template>
+          <ChoroplethMapAnnotation :latitude="44.836151" :longitude="-0.580816" placement="righttop" class="text-center">
+            Bordeaux<br /><small>▼</small>
+          </ChoroplethMapAnnotation>
+        </ChoroplethMap>
+        <p class="text-end mt-2 mb-0">
+          <small><a href="https://www.data.gouv.fr/fr/datasets/campagnes-viti-vinicoles-depuis-2011/" target="_blank">Source: data.gouv.fr</a></small>
+        </p>
+      </div>
+    `
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: `
+Use a different TopoJSON to build maps of specific regions. This example shows French departments with wine stock data.
+
+Configure the TopoJSON source with:
+- \`topojsonUrl\`: Path to the TopoJSON file
+- \`topojsonObjects\`: Name of the objects collection in the TopoJSON
+- \`topojsonObjectsPath\`: Property path to match with your data keys
+        `
+      }
+    }
+  }
+}
+
+export const SphericalProjection: Story = {
+  name: 'Spherical Projection',
+  args: {
+    center: [33.435499, 35.167406],
+    projection: geoOrthographic,
+    zoomMin: 0.9,
+    color: '#aaf',
+    outlineColor: '#fff',
+    graticuleColor: '#333',
+    graticule: true,
+    hideLegend: true,
+    outline: true,
+    spherical: true,
+    zoomable: true
+  },
+  render: args => ({
+    components: { ChoroplethMap, ChoroplethMapAnnotation },
+    setup: () => ({ args }),
+    template: `
+      <div class="bg-dark p-4">
+        <ChoroplethMap v-bind="args">
+          <ChoroplethMapAnnotation
+            :latitude="35.167406"
+            :longitude="33.435499"
+            :height="15"
+            :width="15"
+            class="text-center"
+            drop-shadow="none"
+            scale
+          >
+            <div class="border border-warning" style="height: 15px; width: 15px"></div>
+          </ChoroplethMapAnnotation>
+        </ChoroplethMap>
+      </div>
+    `
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: `
+Use a custom D3 projection for different map views. This example uses \`geoOrthographic\` for a globe-like appearance.
+
+\`\`\`javascript
+import { geoOrthographic } from 'd3-geo'
+
+// Props
+{
   projection: geoOrthographic,
-  zoomMin: 0.9,
-  color: '#aaf',
-  outlineColor: '#fff',
-  graticuleColor: '#333',
-  graticule: true,
-  hideLegend: true,
-  outline: true,
   spherical: true,
-  zoomable: true
+  graticule: true,
+  outline: true
+}
+\`\`\`
+        `
+      }
+    }
+  }
+}
+
+export const HatchEmpty: Story = {
+  args: {
+    data: motorVehiclesPer1000people,
+    hatchEmpty: true
+  },
+  render: args => ({
+    components: { ChoroplethMap },
+    setup: () => ({ args }),
+    template: `<ChoroplethMap v-bind="args" />`
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: 'Use `hatchEmpty` to show a pattern on countries with no data.'
+      }
+    }
+  }
+}
+
+export const Zoomable: Story = {
+  args: {
+    data: motorVehiclesPer1000people,
+    hatchEmpty: true,
+    zoomable: true
+  },
+  render: args => ({
+    components: { ChoroplethMap },
+    setup: () => ({ args }),
+    template: `<ChoroplethMap v-bind="args" />`
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: 'Enable zoom and pan with the `zoomable` prop.'
+      }
+    }
+  }
+}
+
+export const Clickable: Story = {
+  args: {
+    data: wineStockByDepartment,
+    clickable: true,
+    zoomable: true,
+    topojsonUrl: './assets/topojson/france-departments.json',
+    topojsonObjects: 'departements',
+    topojsonObjectsPath: 'properties.code'
+  },
+  render: args => ({
+    components: { ChoroplethMap },
+    setup: () => ({ args }),
+    template: `<ChoroplethMap v-bind="args" />`
+  }),
+  parameters: {
+    docs: {
+      description: {
+        story: 'Enable clicking on features to zoom with the `clickable` prop.'
+      }
+    }
+  }
 }
--- tests/unit/components/SlideUpDown.spec.ts
+++ tests/unit/components/SlideUpDown.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import SlideUpDown from '@/components/SlideUpDown.vue'
 
 describe('SlideUpDown', () => {
