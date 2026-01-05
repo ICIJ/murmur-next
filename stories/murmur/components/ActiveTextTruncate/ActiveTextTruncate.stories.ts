83e73a6f51263fc69611224062624ce0a014e9a9
Pierre Romera <hello@pirhoo.com>
2026-01-05 09:59:27 +0000

    refactor(stories): convert basic component stories to CSF3

--- stories/murmur/components/ActiveTextTruncate/ActiveTextTruncate.stories.ts
+++ stories/murmur/components/ActiveTextTruncate/ActiveTextTruncate.stories.ts
@@ -1,33 +1,36 @@
 import { ActiveTextTruncate } from '@/components'
-import { StoryObj } from '@storybook/vue3-vite'
 import { bgStripedDecorator } from '../../decorators'
-
-type Story = StoryObj<typeof ActiveTextTruncate>
-const Template: Story = (args: any, { argTypes }: any) => ({
-  components: { ActiveTextTruncate },
-  props: Object.keys(argTypes),
-
-  setup() {
-    return { args }
-  },
-  template:
-    '<active-text-truncate v-bind="args">https://www.icij.org/investigations/luanda-leaks/banking-documents-reveal-consulting-giants-cash-windfall-under-angolan-billionaire-isabel-dos-santos/</active-text-truncate>'
-})
-
-export const Default = Template.bind({})
-Default.args = { direction: 'ltr' }
-export const RightToLeft = Template.bind({})
-RightToLeft.args = { direction: 'rtl' }
 
 export default {
   title: 'Murmur/components/ActiveTextTruncate/ActiveTextTruncate',
   component: ActiveTextTruncate,
-  decorators: [bgStripedDecorator],
   tags: ['autodocs'],
+  decorators: [bgStripedDecorator],
   argTypes: {
     direction: { control: 'inline-radio', options: ['ltr', 'rtl'] },
-    ppms: { control: { type: 'number', min: 0 } },
-    fadingMinWidth: { control: { type: 'number', min: 0 } },
-    fadingMaxWidth: { control: { type: 'number', min: 0 } }
+    ppms: { control: 'number' },
+    fadingMinWidth: { control: 'number' },
+    fadingMaxWidth: { control: 'number' }
   }
+}
+
+const longUrl =
+  'https://www.icij.org/investigations/luanda-leaks/banking-documents-reveal-consulting-giants-cash-windfall-under-angolan-billionaire-isabel-dos-santos/'
+
+export const Default = {
+  args: { direction: 'ltr' },
+  render: (args: any) => ({
+    components: { ActiveTextTruncate },
+    setup: () => ({ args, longUrl }),
+    template: '<ActiveTextTruncate v-bind="args">{{ longUrl }}</ActiveTextTruncate>'
+  })
+}
+
+export const RightToLeft = {
+  args: { direction: 'rtl' },
+  render: (args: any) => ({
+    components: { ActiveTextTruncate },
+    setup: () => ({ args, longUrl }),
+    template: '<ActiveTextTruncate v-bind="args">{{ longUrl }}</ActiveTextTruncate>'
+  })
 }
--- stories/murmur/components/ContentPlaceholder/ContentPlaceholder.stories.ts
+++ stories/murmur/components/ContentPlaceholder/ContentPlaceholder.stories.ts
@@ -1,5 +1,4 @@
 import { ContentPlaceholder } from '@/components'
-import { StoryObj } from '@storybook/vue3-vite'
 import { ContentPlaceholderRows } from '@/utils/placeholderTypes'
 
 export default {
@@ -9,20 +8,7 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof ContentPlaceholder>
-const Template: Story = (args: any) => ({
-  components: { ContentPlaceholder },
-  setup() {
-    return { args }
-  },
-  template: '<ContentPlaceholder v-bind="args" />'
-})
-
-export const Default = Template.bind({})
-Default.args = {}
-
-export const Columns = Template.bind({})
-const rows: ContentPlaceholderRows = [
+const columnRows: ContentPlaceholderRows = [
   {
     height: '1em',
     boxes: [
@@ -32,18 +18,28 @@ const rows: ContentPlaceholderRows = [
     ]
   }
 ]
-Columns.args = { rows }
-export const FourRows = Template.bind({})
+
 const fourRowsDecorator = () => ({
   components: { ContentPlaceholder },
   template: `
-        <div class="card card-xs py-2 px-3 mx-auto m-4">
-            <story/>
-            <story/>
-            <story/>
-            <story/>
-        </div>
-    `
+    <div class="card card-xs py-2 px-3 mx-auto m-4">
+      <story/>
+      <story/>
+      <story/>
+      <story/>
+    </div>
+  `
 })
-FourRows.decorators = [fourRowsDecorator]
-FourRows.args = { class: 'my-1' }
+
+export const Default = {
+  args: {}
+}
+
+export const Columns = {
+  args: { rows: columnRows }
+}
+
+export const FourRows = {
+  args: { class: 'my-1' },
+  decorators: [fourRowsDecorator]
+}
