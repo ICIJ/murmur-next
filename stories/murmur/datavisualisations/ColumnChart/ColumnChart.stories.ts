d436e634f8401ceed36596e4e1a379a1afd48aa1
Pierre Romera <hello@pirhoo.com>
2026-01-05 10:00:58 +0000

    refactor(stories): convert chart stories to CSF3 format

--- stories/murmur/datavisualisations/ColumnChart/ColumnChart.stories.ts
+++ stories/murmur/datavisualisations/ColumnChart/ColumnChart.stories.ts
@@ -1,5 +1,5 @@
+import { ref } from 'vue'
 import { ColumnChart } from '@/datavisualisations'
-import { StoryObj } from '@storybook/vue3-vite'
 import { leakSizeDecorator } from '../../decorators'
 
 export default {
@@ -9,19 +9,9 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof ColumnChart>
-const Template: Story = (args: any) => ({
-  components: { ColumnChart },
-  setup() {
-    return { args }
-  },
-  template: `
-    <ColumnChart v-bind="args"/>`
-})
+const dataUrl =
+  'https://gist.githubusercontent.com/pirhoo/259a1a5159db4a665d0c043fac71beef/raw/e74087b06cd12be2b2d3a8ca995730e38719cd4b/colums-incidents.json'
 
-const fixedHeight = 300
-const dataUrl
-  = 'https://gist.githubusercontent.com/pirhoo/259a1a5159db4a665d0c043fac71beef/raw/e74087b06cd12be2b2d3a8ca995730e38719cd4b/colums-incidents.json'
 const discreteData = [
   { leak: 'Paradise Papers', size: 1.4 * 1e3 },
   { leak: 'Panama Papers', size: 2.6 * 1e3 },
@@ -32,87 +22,88 @@ const discreteData = [
 
 const leakInjuriesDecorator = () => ({
   template: `
-  <h4>Breast implant companies buried evidence of injuries for years</h4>
-  <p class="text-muted">
-    Incidents were reported as routine events that did not require public disclosure. After the U.S. Food and Drug Administration tightened enforcement of its reporting rules in 2017, reports of injuries soared.
-  </p>
-  <story/>
-  <p class="text-muted small">
-    Note: 2018 data includes January to June. Source: U.S. Food and Drug Administration, ICIJ analysis
-  </p>`
-})
-export const Default = Template.bind({})
-
-Default.decorators = [leakInjuriesDecorator]
-Default.args = {
-  data: dataUrl,
-  xAxisTickCollapse: true,
-  hover: true,
-  hoverIcon: 'info'
-}
-
-export const TooltipSlot: Story = (args: any) => ({
-  components: { ColumnChart },
-  setup() {
-    return { args }
-  },
-  template: `
-    <column-chart v-bind="args">
-      <template #tooltip="{ datum: { size } }">
-        {{ size }}GB
-      </template>
-    </column-chart>`
-})
-
-TooltipSlot.decorators = [leakSizeDecorator]
-
-TooltipSlot.args = {
-  data: discreteData,
-  seriesName: 'size',
-  timeseriesKey: 'leak',
-  yAxisYTicks: 4,
-  maxValue: 3000
-}
-
-export const DynamicHeight = TooltipSlot.bind({})
-
-DynamicHeight.args = {
-  data: discreteData,
-  seriesName: 'size',
-  timeseriesKey: 'leak',
-  yAxisYTicks: 4,
-  maxValue: 3000,
-  stripped: true,
-  fixedHeight: fixedHeight
-}
-
-export const DynamicData: Story = () => ({
-  components: { ColumnChart },
-  data() {
-    return {
-      data: [] as { date: string, value: number }[]
-    }
-  },
-  beforeMount() {
-    this.generateData()
-  },
-  methods: {
-    generateData() {
-      const data: { date: string, value: number }[] = []
-      const startYear = 1995 + ~(Math.random() * 10)
-      const groups = ~~(5 + Math.random() * 15)
-      for (let g = 0; g < groups; g++) {
-        const date = `${startYear + g}`
-        const value = ~~(Math.random() * 100)
-        data.push({ date, value })
-      }
-      this.data = data
-    }
-  },
-  template: `
-    <button class="btn btn-primary" @click="generateData()">
-      Randomize
-    </button>
-    <column-chart :data="data" :fixed-height="400" stripped />
+    <h4>Breast implant companies buried evidence of injuries for years</h4>
+    <p class="text-muted">
+      Incidents were reported as routine events that did not require public disclosure.
+      After the U.S. Food and Drug Administration tightened enforcement of its reporting rules in 2017, reports of injuries soared.
+    </p>
+    <story/>
+    <p class="text-muted small">
+      Note: 2018 data includes January to June. Source: U.S. Food and Drug Administration, ICIJ analysis
+    </p>
   `
 })
+
+export const Default = {
+  args: {
+    data: dataUrl,
+    xAxisTickCollapse: true,
+    hover: true,
+    hoverIcon: 'info'
+  },
+  decorators: [leakInjuriesDecorator]
+}
+
+export const TooltipSlot = {
+  args: {
+    data: discreteData,
+    seriesName: 'size',
+    timeseriesKey: 'leak',
+    yAxisYTicks: 4,
+    maxValue: 3000
+  },
+  decorators: [leakSizeDecorator],
+  render: (args: any) => ({
+    components: { ColumnChart },
+    setup: () => ({ args }),
+    template: `
+      <ColumnChart v-bind="args">
+        <template #tooltip="{ datum: { size } }">
+          {{ size }}GB
+        </template>
+      </ColumnChart>
+    `
+  })
+}
+
+export const DynamicHeight = {
+  args: {
+    data: discreteData,
+    seriesName: 'size',
+    timeseriesKey: 'leak',
+    yAxisYTicks: 4,
+    maxValue: 3000,
+    stripped: true,
+    fixedHeight: 300
+  }
+}
+
+export const DynamicData = {
+  render: () => ({
+    components: { ColumnChart },
+    setup() {
+      const data = ref<{ date: string; value: number }[]>([])
+
+      function generateData() {
+        const newData: { date: string; value: number }[] = []
+        const startYear = 1995 + ~~(Math.random() * 10)
+        const groups = ~~(5 + Math.random() * 15)
+        for (let g = 0; g < groups; g++) {
+          newData.push({
+            date: `${startYear + g}`,
+            value: ~~(Math.random() * 100)
+          })
+        }
+        data.value = newData
+      }
+
+      generateData()
+
+      return { data, generateData }
+    },
+    template: `
+      <button class="btn btn-primary" @click="generateData()">Randomize</button>
+      <ColumnChart :data="data" :fixed-height="400" stripped />
+    `
+  })
+}
--- stories/murmur/datavisualisations/StackedColumnChart/StackedColumnChart.stories.ts
+++ stories/murmur/datavisualisations/StackedColumnChart/StackedColumnChart.stories.ts
@@ -1,5 +1,4 @@
 import { StackedColumnChart } from '@/datavisualisations'
-import { StoryObj } from '@storybook/vue3-vite'
 import { leakSizeDecorator } from '../../decorators'
 import { humanReadableGb } from '../../utils'
 
@@ -10,142 +9,122 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof StackedColumnChart>
+const incidentReportsUrl =
+  'https://gist.githubusercontent.com/pirhoo/4055e8d1ee3016805eaf1d2feabdd895/raw/a3d2ba8e9d19fcd9fc659dab50ec075248178238/stacked-colums-incidents.json'
+const incidentReportsGroups = ['Deaths', 'Injuries', 'Malfunctions']
 
-const Template: Story = (args: any) => ({
-  components: { StackedColumnChart },
-  setup() {
-    return { args }
-  },
-  template: `<StackedColumnChart v-bind="args" />`
-})
+const icijStaff = [
+  { city: 'Washington DC', developers: 1, journalists: 6, devops: 1, finance: 1 },
+  { city: 'Paris ', developers: 5, journalists: 1, devops: 0, finance: 0 },
+  { city: 'Madrid ', developers: 1, journalists: 0, devops: 4, finance: 0 },
+  { city: 'New York City', developers: 0, journalists: 3, devops: 0, finance: 1 },
+  { city: 'Sydney', developers: 0, journalists: 2, devops: 0, finance: 1 }
+]
 
-const data = {
-  incidentReportsUrl:
-    'https://gist.githubusercontent.com/pirhoo/4055e8d1ee3016805eaf1d2feabdd895/raw/a3d2ba8e9d19fcd9fc659dab50ec075248178238/stacked-colums-incidents.json',
-  incidentReportsGroups: ['Deaths', 'Injuries', 'Malfunctions'],
-  icijStaff: [
-    {
-      city: 'Washington DC',
-      developers: 1,
-      journalists: 6,
-      devops: 1,
-      finance: 1
-    },
-    { city: 'Paris ', developers: 5, journalists: 1, devops: 0, finance: 0 },
-    { city: 'Madrid ', developers: 1, journalists: 0, devops: 4, finance: 0 },
-    {
-      city: 'New York City',
-      developers: 0,
-      journalists: 3,
-      devops: 0,
-      finance: 1
-    },
-    { city: 'Sydney', developers: 0, journalists: 2, devops: 0, finance: 1 }
-  ],
-  leaksSize: [
-    { leak: 'Offshore Leaks (2013)', size: 260 },
-    { leak: 'Panama Papers (2016)', size: 2.6 * 1e3 },
-    { leak: 'Paradise Papers (2017)', size: 1.4 * 1e3 }
-  ]
-}
+const leaksSize = [
+  { leak: 'Offshore Leaks (2013)', size: 260 },
+  { leak: 'Panama Papers (2016)', size: 2.6 * 1e3 },
+  { leak: 'Paradise Papers (2017)', size: 1.4 * 1e3 }
+]
 
 const leakIncidentsDecorator = () => ({
   template: `
-  <h4>
-    Breast implant companies buried evidence of injuries for years
-  </h4>
-  <p class="text-muted">
-    Incidents were reported as routine events that did not require public disclosure. After the FDA tightened enforcement of its reporting rules in 2017, reports of injuries soared.
-  </p>
-
-   <story/>
-  <p class="text-muted small">
-    Source: U.S. Food and Drug Administration, ICIJ analysis
-  </p>`
+    <h4>Breast implant companies buried evidence of injuries for years</h4>
+    <p class="text-muted">
+      Incidents were reported as routine events that did not require public disclosure. After the FDA tightened enforcement of its reporting rules in 2017, reports of injuries soared.
+    </p>
+    <story/>
+    <p class="text-muted small">Source: U.S. Food and Drug Administration, ICIJ analysis</p>
+  `
 })
 
 const icijOfficesDecorator = () => ({
   template: `
-  <h4>
-    ICIJ Staff by office
-  </h4>
-  <p class="text-muted">As of April 2021. This list is more or less accurate.</p>
-  <story/>
-`
+    <h4>ICIJ Staff by office</h4>
+    <p class="text-muted">As of April 2021. This list is more or less accurate.</p>
+    <story/>
+  `
 })
 
-export const Default = Template.bind({})
-Default.decorators = [leakIncidentsDecorator]
-Default.args = {
-  data: data.incidentReportsUrl,
-  groups: data.incidentReportsGroups,
-  yAxisTickFormat: ',.0f'
+export const Default = {
+  args: {
+    data: incidentReportsUrl,
+    groups: incidentReportsGroups,
+    yAxisTickFormat: ',.0f'
+  },
+  decorators: [leakIncidentsDecorator]
 }
 
-export const NoDirectLabeling = Template.bind({})
-NoDirectLabeling.decorators = [leakIncidentsDecorator]
-NoDirectLabeling.args = {
-  data: data.incidentReportsUrl,
-  groups: data.incidentReportsGroups,
-  noDirectLabeling: true
+export const NoDirectLabeling = {
+  args: {
+    data: incidentReportsUrl,
+    groups: incidentReportsGroups,
+    noDirectLabeling: true
+  },
+  decorators: [leakIncidentsDecorator]
 }
 
-export const ColumnLabelField = Template.bind({})
-ColumnLabelField.decorators = [icijOfficesDecorator]
-ColumnLabelField.args = {
-  data: data.icijStaff,
-  labelField: 'city'
+export const ColumnLabelField = {
+  args: {
+    data: icijStaff,
+    labelField: 'city'
+  },
+  decorators: [icijOfficesDecorator]
 }
 
-export const MaxValue = Template.bind({})
-MaxValue.decorators = [icijOfficesDecorator]
-MaxValue.args = {
-  data: data.icijStaff,
-  hideEmptyValues: true,
-  labelField: 'city',
-  maxValue: 10
+export const MaxValue = {
+  args: {
+    data: icijStaff,
+    hideEmptyValues: true,
+    labelField: 'city',
+    maxValue: 10
+  },
+  decorators: [icijOfficesDecorator]
 }
 
-export const NoTooltips = Template.bind({})
-NoTooltips.decorators = [icijOfficesDecorator]
-NoTooltips.args = {
-  data: data.icijStaff,
-  noTooltips: true,
-  hideEmptyValues: true,
-  labelField: 'city',
-  maxValue: 10
+export const NoTooltips = {
+  args: {
+    data: icijStaff,
+    noTooltips: true,
+    hideEmptyValues: true,
+    labelField: 'city',
+    maxValue: 10
+  },
+  decorators: [icijOfficesDecorator]
 }
 
-export const SingleValueProp = Template.bind({})
-SingleValueProp.decorators = [leakSizeDecorator]
-SingleValueProp.args = {
-  data: data.leaksSize,
-  yAxisTickFormat: humanReadableGb,
-  noTooltips: true,
-  labelField: 'leak',
-  maxValue: 3000
+export const SingleValueProp = {
+  args: {
+    data: leaksSize,
+    yAxisTickFormat: humanReadableGb,
+    noTooltips: true,
+    labelField: 'leak',
+    maxValue: 3000
+  },
+  decorators: [leakSizeDecorator]
 }
 
-export const HideLegend = Template.bind({})
-HideLegend.decorators = [leakSizeDecorator]
-HideLegend.args = {
-  data: data.leaksSize,
-  yAxisTickFormat: humanReadableGb,
-  noTooltips: true,
-  labelField: 'leak',
-  maxValue: 3000,
-  hideLegend: true
+export const HideLegend = {
+  args: {
+    data: leaksSize,
+    yAxisTickFormat: humanReadableGb,
+    noTooltips: true,
+    labelField: 'leak',
+    maxValue: 3000,
+    hideLegend: true
+  },
+  decorators: [leakSizeDecorator]
 }
 
-export const BarMaxWidth = Template.bind({})
-BarMaxWidth.decorators = [leakSizeDecorator]
-BarMaxWidth.args = {
-  data: data.leaksSize,
-  yAxisTickFormat: humanReadableGb,
-  noTooltips: true,
-  labelField: 'leak',
-  maxValue: 3000,
-  barMaxWidth: '50%',
-  hideLegend: true
+export const BarMaxWidth = {
+  args: {
+    data: leaksSize,
+    yAxisTickFormat: humanReadableGb,
+    noTooltips: true,
+    labelField: 'leak',
+    maxValue: 3000,
+    barMaxWidth: '50%',
+    hideLegend: true
+  },
+  decorators: [leakSizeDecorator]
 }
