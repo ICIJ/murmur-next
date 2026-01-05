768727a20378a713de335ecda34ae0cd7a167163
Pierre Romera <hello@pirhoo.com>
2026-01-05 10:00:40 +0000

    refactor(stories): convert FormControl stories to CSF3

--- stories/murmur/components/Form/FormControl/FormControlDigits.stories.ts
+++ stories/murmur/components/Form/FormControl/FormControlDigits.stories.ts
@@ -1,28 +1,21 @@
 import { FormControlDigits } from '@/components'
-import { StoryObj } from '@storybook/vue3-vite'
 
 export default {
   title: 'Murmur/components/Form/FormControl/FormControlDigits',
   component: FormControlDigits,
   tags: ['autodocs'],
   argTypes: {
-    length: { type: 'number', min: 0 }
+    length: { control: 'number' }
   }
 }
 
-type Story = StoryObj<typeof FormControlDigits>
-const Template: Story = (args: any) => ({
-  components: { FormControlDigits },
-  setup() {
-    return { args }
-  },
-  template: '<FormControlDigits v-bind="args" />'
-})
+export const Default = {
+  args: {}
+}
 
-export const Default = Template.bind({})
-Default.args = {}
-export const FourDigits = Template.bind({})
-FourDigits.args = {
-  length: 4,
-  modelValue: 2017
+export const FourDigits = {
+  args: {
+    length: 4,
+    modelValue: 2017
+  }
 }
