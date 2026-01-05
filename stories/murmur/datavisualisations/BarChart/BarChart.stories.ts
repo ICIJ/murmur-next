0e5e729b9d32f96e85aabe9acdf62ffa13c7d5a5
Pierre Romera <hello@pirhoo.com>
2026-01-05 10:00:58 +0000

    refactor(stories): convert chart stories to CSF3 format

--- stories/murmur/datavisualisations/BarChart/BarChart.stories.ts
+++ stories/murmur/datavisualisations/BarChart/BarChart.stories.ts
@@ -1,5 +1,4 @@
 import { BarChart } from '@/datavisualisations'
-import { StoryObj } from '@storybook/vue3-vite'
 
 export default {
   title: 'Murmur/datavisualisations/BarChart/BarChart',
@@ -8,48 +7,33 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof BarChart>
-const Template: Story = (args: any) => ({
-  components: { BarChart },
-  setup() {
-    return { args }
-  },
-  template: `<BarChart v-bind="args" />`
-})
 const breastImplantDecorator = () => ({
   template: `
     <h4>Breast implant patients kept in the dark about potential risks</h4>
     <p class="text-muted">
-    More than 300 people around the world who responded to an ICIJ survey said they were not aware of the dangers of breast implants prior to their surgeries.
+      More than 300 people around the world who responded to an ICIJ survey said they were not aware of the dangers of breast implants prior to their surgeries.
     </p>
-    <story/>`
+    <story/>
+  `
 })
-export const Default = Template.bind({})
-Default.decorators = [breastImplantDecorator]
-Default.args = {
-  data: 'https://gist.githubusercontent.com/pirhoo/2308336d5f067ef7d84fec348fd63e29/raw/c0135f11e54e757187163dd0722b149a456c64b1/bars-icij-survey.json'
-}
+
 const dataWithHighlight = [
-  {
-    label: 'Warned about local complications',
-    value: 52
-  },
-  {
-    label: 'Not warned',
-    value: 42,
-    highlight: true
-  },
-  {
-    label: 'Warned thoroughly',
-    value: 1
-  },
-  {
-    label: 'Other',
-    value: 6
-  }
+  { label: 'Warned about local complications', value: 52 },
+  { label: 'Not warned', value: 42, highlight: true },
+  { label: 'Warned thoroughly', value: 1 },
+  { label: 'Other', value: 6 }
 ]
-export const Highlight = Template.bind({})
-Highlight.decorators = [breastImplantDecorator]
-Highlight.args = {
-  data: dataWithHighlight
+
+export const Default = {
+  args: {
+    data: 'https://gist.githubusercontent.com/pirhoo/2308336d5f067ef7d84fec348fd63e29/raw/c0135f11e54e757187163dd0722b149a456c64b1/bars-icij-survey.json'
+  },
+  decorators: [breastImplantDecorator]
+}
+
+export const Highlight = {
+  args: {
+    data: dataWithHighlight
+  },
+  decorators: [breastImplantDecorator]
 }
--- stories/murmur/datavisualisations/StackedBarChart/StackedBarChart.stories.ts
+++ stories/murmur/datavisualisations/StackedBarChart/StackedBarChart.stories.ts
@@ -1,5 +1,4 @@
 import { StackedBarChart } from '@/datavisualisations'
-import { StoryObj } from '@storybook/vue3-vite'
 import { humanReadableGb } from '../../utils'
 
 export default {
@@ -9,203 +8,157 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof StackedBarChart>
+const incidentReports = [
+  { label: 'Medtronic PLC', injury: 71444, death: 1828 },
+  { label: 'Abbott Laboratories', injury: 40200, death: 2816 },
+  { label: 'Johnson & Johnson', injury: 25863, death: 104 },
+  { label: 'Boston Scientific', injury: 20509, death: 725 },
+  { label: 'Zimmer Biomet Holdings', injury: 15733, death: 146 },
+  { label: 'Tandem Diabetes Care, Inc.', injury: 13658, death: 26 },
+  { label: 'Stryker', injury: 5102, death: 90 },
+  { label: 'Becton, Dickinson and Company', injury: 3569, death: 130 },
+  { label: 'DexCom, Inc.', injury: 1198, death: 25 },
+  { label: 'Philips', injury: 450, death: 268 }
+]
 
-const Template: Story = (args: any) => ({
-  components: { StackedBarChart },
-  setup() {
-    return { args }
-  },
-  template: `<StackedBarChart v-bind="args" >
-      {{args['header-right'].toString()}}
-      <template #header-left v-if="args['header-left']">${args['header-left']}</template>
-      <template #header-right v-if="args['header-right']!==null">${args['header-right']?.({ sortBy: args.sortBy }) ?? ''}</template>
-    </StackedBarChart>`
-})
+const leakSizes = [
+  { label: 'Paradise Papers', value: 1.4 * 1e3 },
+  { label: 'Panama Papers', value: 2.6 * 1e3 },
+  { label: 'Swiss Leaks', value: 3.3 },
+  { label: 'LuxLeaks', value: 4 },
+  { label: 'Offshore Leaks', value: 260 }
+]
+
+const moviesUrl =
+  'https://gist.githubusercontent.com/pirhoo/20ce1b795555210c926967a291f8a7ad/raw/13d972b7d2b98b174c33fff38aac2b7d69c85fa7/stacked-bars-movies.json'
+const movieGroups = ['Budget', 'Box Office']
+const sortKeys = ['movie', 'budget', 'box_office']
 
 const leakIncidentsDecorator = () => ({
   template: `
-  <h4>
-    Medical device companies reported tens of thousands of incidents in 2017
-  </h4>
-   <p class="text-muted">
-    Companies must report when a patient has potentially been hurt or killed by one of their medical devices.
-  </p>
-  <story/>
-  <p class="text-muted small">
-    Note: The companies shown here are 10 of the biggest participants in the medical device industry. Numbers for Becton, Dickinson and Company include adverse events reported by C. R. Bard, which was acquired in 2017. Source: U.S. Food and Drug Administration, ICIJ analysis.
-  </p>
-  <p class="text-muted small">
-    Source: ICIJ.
-  </p>`
+    <h4>Medical device companies reported tens of thousands of incidents in 2017</h4>
+    <p class="text-muted">
+      Companies must report when a patient has potentially been hurt or killed by one of their medical devices.
+    </p>
+    <story/>
+    <p class="text-muted small">
+      Note: The companies shown here are 10 of the biggest participants in the medical device industry.
+      Numbers for Becton, Dickinson and Company include adverse events reported by C. R. Bard, which was acquired in 2017.
+      Source: U.S. Food and Drug Administration, ICIJ analysis.
+    </p>
+  `
 })
-
-const data = {
-  fixedHeight: 400,
-  incidentReports: [
-    {
-      label: 'Medtronic PLC',
-      injury: 71444,
-      death: 1828
-    },
-    {
-      label: 'Abbott Laboratories',
-      injury: 40200,
-      death: 2816
-    },
-    {
-      label: 'Johnson & Johnson',
-      injury: 25863,
-      death: 104
-    },
-    {
-      label: 'Boston Scientific',
-      injury: 20509,
-      death: 725
-    },
-    {
-      label: 'Zimmer Biomet Holdings',
-      injury: 15733,
-      death: 146
-    },
-    {
-      label: 'Tandem Diabetes Care, Inc.',
-      injury: 13658,
-      death: 26
-    },
-    {
-      label: 'Stryker',
-      injury: 5102,
-      death: 90
-    },
-    {
-      label: 'Becton, Dickinson and Company',
-      injury: 3569,
-      death: 130
-    },
-    {
-      label: 'DexCom, Inc.',
-      injury: 1198,
-      death: 25
-    },
-    {
-      label: 'Philips',
-      injury: 450,
-      death: 268
-    }
-  ],
-  moviesUrl:
-    'https://gist.githubusercontent.com/pirhoo/20ce1b795555210c926967a291f8a7ad/raw/13d972b7d2b98b174c33fff38aac2b7d69c85fa7/stacked-bars-movies.json',
-  isRelative: true,
-  sortKeys: ['movie', 'budget', 'box_office'],
-  sortKey: ['movie'],
-  groups: ['Budget', 'Box Office'],
-  leakSizes: [
-    { label: 'Paradise Papers', value: 1.4 * 1e3 },
-    { label: 'Panama Papers', value: 2.6 * 1e3 },
-    { label: 'Swiss Leaks', value: 3.3 },
-    { label: 'LuxLeaks', value: 4 },
-    { label: 'Offshore Leaks', value: 260 }
-  ]
-}
-
-export const Default = Template.bind({})
-Default.decorators = [leakIncidentsDecorator]
-Default.args = {
-  data: data.incidentReports
-}
-
-export const LabelAbove = Template.bind({})
-LabelAbove.decorators = [leakIncidentsDecorator]
-LabelAbove.args = {
-  data: data.incidentReports,
-  labelAbove: true
-}
 
 const leaksSizeDecorator = () => ({
   template: `
-   <h4>Leaks size</h4>
-  <p class="text-muted">
-    Size of each leak in GB.
-  </p>
-  <story/>`
+    <h4>Leaks size</h4>
+    <p class="text-muted">Size of each leak in GB.</p>
+    <story/>
+  `
 })
 
-export const FixedHeightHideLegend = Template.bind({})
-FixedHeightHideLegend.decorators = [leaksSizeDecorator]
-FixedHeightHideLegend.args = {
-  data: data.leakSizes,
-  fixedHeight: 400,
-  xAxisTickFormat: humanReadableGb,
-  hideLegend: true
+export const Default = {
+  args: { data: incidentReports },
+  decorators: [leakIncidentsDecorator]
 }
 
-export const HideEmptyValues = Template.bind({})
-HideEmptyValues.decorators = []
-HideEmptyValues.args = {
-  data: data.moviesUrl,
-  labelField: 'movie',
-  groups: data.groups,
-  hideEmptyValues: true
+export const LabelAbove = {
+  args: {
+    data: incidentReports,
+    labelAbove: true
+  },
+  decorators: [leakIncidentsDecorator]
 }
 
-export const SortByKey = Template.bind({})
-SortByKey.decorators = []
-SortByKey.args = {
-  data: data.moviesUrl,
-  labelField: 'movie',
-  sortBy: data.sortKey,
-  groups: data.groups
-}
-SortByKey.argTypes = {
-  sortBy: { control: 'select', options: data.sortKeys }
+export const FixedHeightHideLegend = {
+  args: {
+    data: leakSizes,
+    fixedHeight: 400,
+    xAxisTickFormat: humanReadableGb,
+    hideLegend: true
+  },
+  decorators: [leaksSizeDecorator]
 }
 
-export const RelativeValues = Template.bind({})
-RelativeValues.decorators = []
-RelativeValues.args = {
-  data: data.moviesUrl,
-  labelField: 'movie',
-  sortBy: data.sortKey,
-  groups: data.groups,
-  relative: true
+export const HideEmptyValues = {
+  args: {
+    data: moviesUrl,
+    labelField: 'movie',
+    groups: movieGroups,
+    hideEmptyValues: true
+  }
 }
 
-export const HeaderLeft = Template.bind({})
-HeaderLeft.decorators = [leakIncidentsDecorator]
-HeaderLeft.args = {
-  'data': data.leakSizes,
-  'fixedHeight': 400,
-  'xAxisTickFormat': humanReadableGb,
-  'hideLegend': false,
-  'header-left': '<h1>Header Left</h1>'
+export const SortByKey = {
+  args: {
+    data: moviesUrl,
+    labelField: 'movie',
+    sortBy: ['movie'],
+    groups: movieGroups
+  },
+  argTypes: {
+    sortBy: { control: 'select', options: sortKeys }
+  }
 }
 
-HeaderLeft.argTypes = {
-  sortBy: { control: 'select', options: data.sortKeys }
+export const RelativeValues = {
+  args: {
+    data: moviesUrl,
+    labelField: 'movie',
+    sortBy: ['movie'],
+    groups: movieGroups,
+    relative: true
+  }
 }
 
-export const HeaderRight = Template.bind({})
-HeaderRight.decorators = [leakIncidentsDecorator]
-HeaderRight.args = {
-  'data': data.moviesUrl,
-  'labelField': 'movie',
-  'sortBy': data.sortKey,
-  'hideEmptyValues': true,
-  'relative': true,
-  'groups': data.groups,
-  'hideLegend': true,
-  'header-right': ({
-    sortBy
-  }: {
-    sortBy: string
-  }) => `<div class="ms-auto d-flex  align-items-center p-0">
-        <label class="m-2 d-flex align-items-center">
-          Sort by  ${sortBy}
-        </label>
-      </div>`
+export const HeaderLeft = {
+  args: {
+    data: leakSizes,
+    fixedHeight: 400,
+    xAxisTickFormat: humanReadableGb,
+    hideLegend: false
+  },
+  decorators: [leakIncidentsDecorator],
+  render: (args: any) => ({
+    components: { StackedBarChart },
+    setup: () => ({ args }),
+    template: `
+      <StackedBarChart v-bind="args">
+        <template #header-left>
+          <h1>Header Left</h1>
+        </template>
+      </StackedBarChart>
+    `
+  })
 }
 
-HeaderRight.argTypes = {
-  sortBy: { control: 'select', options: data.sortKeys }
+export const HeaderRight = {
+  args: {
+    data: moviesUrl,
+    labelField: 'movie',
+    sortBy: ['movie'],
+    hideEmptyValues: true,
+    relative: true,
+    groups: movieGroups,
+    hideLegend: true
+  },
+  decorators: [leakIncidentsDecorator],
+  argTypes: {
+    sortBy: { control: 'select', options: sortKeys }
+  },
+  render: (args: any) => ({
+    components: { StackedBarChart },
+    setup: () => ({ args }),
+    template: `
+      <StackedBarChart v-bind="args">
+        <template #header-right>
+          <div class="ms-auto d-flex align-items-center p-0">
+            <label class="m-2 d-flex align-items-center">
+              Sort by {{ args.sortBy }}
+            </label>
+          </div>
+        </template>
+      </StackedBarChart>
+    `
+  })
 }
