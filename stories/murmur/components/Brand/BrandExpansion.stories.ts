83759d1f7733f45d5526e8adbd48ba5f95a5c9e8
Pierre Romera <hello@pirhoo.com>
2026-01-05 09:59:16 +0000

    refactor(stories): convert Brand and Button stories to CSF3

--- stories/murmur/components/Brand/BrandExpansion.stories.ts
+++ stories/murmur/components/Brand/BrandExpansion.stories.ts
@@ -1,5 +1,4 @@
 import { BrandExpansion } from '@/components'
-import { StoryObj } from '@storybook/vue3-vite'
 
 export default {
   title: 'Murmur/components/Brand/BrandExpansion',
@@ -9,32 +8,35 @@ export default {
     mode: { control: 'select', options: ['short', 'medium', 'long'] },
     animated: { control: 'boolean' },
     dark: { control: 'boolean' },
-    color: { control: { type: 'color' } },
-    background: { control: { type: 'color' } }
+    color: { control: 'color' },
+    background: { control: 'color' }
   }
 }
 
-type Story = StoryObj<typeof BrandExpansion>
-const Template: Story = (args: any) => ({
-  components: { BrandExpansion },
-  setup() {
-    return { args }
-  },
-  template: '<BrandExpansion v-bind="args" />'
-})
+export const Default = {
+  args: { mode: 'short' }
+}
 
-export const Default = Template.bind({})
-Default.args = { mode: 'short' }
-export const Medium = Template.bind({})
-Medium.args = { mode: 'medium' }
+export const Medium = {
+  args: { mode: 'medium' }
+}
 
-export const Long = Template.bind({})
-Long.args = { mode: 'long' }
-export const Animated = Template.bind({})
-Animated.args = { animated: true }
-export const WhiteBackground = Template.bind({})
-WhiteBackground.args = { background: '#FFF', color: '#000' }
-export const BlackBackground = Template.bind({})
-BlackBackground.args = { background: '#000', color: '#FFF' }
-export const Dark = Template.bind({})
-Dark.args = { background: '#DDD', dark: true }
+export const Long = {
+  args: { mode: 'long' }
+}
+
+export const Animated = {
+  args: { animated: true }
+}
+
+export const WhiteBackground = {
+  args: { background: '#FFF', color: '#000' }
+}
+
+export const BlackBackground = {
+  args: { background: '#000', color: '#FFF' }
+}
+
+export const Dark = {
+  args: { background: '#DDD', dark: true }
+}
