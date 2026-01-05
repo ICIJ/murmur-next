0730b0ced8242f0b6c58569cfaee77bab2662373
Pierre Romera <hello@pirhoo.com>
2026-01-05 09:59:16 +0000

    refactor(stories): convert Brand and Button stories to CSF3

--- stories/murmur/components/Button/ButtonConfirm.stories.ts
+++ stories/murmur/components/Button/ButtonConfirm.stories.ts
@@ -1,5 +1,4 @@
 import { ButtonConfirm } from '@/components'
-import { StoryObj } from '@storybook/vue3-vite'
 
 export default {
   title: 'Murmur/components/Button/ButtonConfirm',
@@ -8,33 +7,45 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof ButtonConfirm>
-const Template: Story = (args: any) => ({
-  components: { ButtonConfirm },
-  setup() {
-    return { args }
-  },
-  template: `<ButtonConfirm v-bind="args" >Click to confirm</ButtonConfirm>`
-})
-
 function confirmedFn() {
   alert('Confirmed !')
 }
-export const Default = Template.bind({})
-Default.args = {
-  class: 'btn btn-info',
-  confirmed: confirmedFn,
-  noCloseButton: true
+
+export const Default = {
+  args: {
+    class: 'btn btn-info',
+    confirmed: confirmedFn,
+    noCloseButton: true
+  },
+  render: (args: any) => ({
+    components: { ButtonConfirm },
+    setup: () => ({ args }),
+    template: '<ButtonConfirm v-bind="args">Click to confirm</ButtonConfirm>'
+  })
 }
-export const WithDescription = Template.bind({})
-WithDescription.args = {
-  class: 'btn btn-info',
-  description: 'Do, or do not. There is no try.'
+
+export const WithDescription = {
+  args: {
+    class: 'btn btn-info',
+    description: 'Do, or do not. There is no try.'
+  },
+  render: (args: any) => ({
+    components: { ButtonConfirm },
+    setup: () => ({ args }),
+    template: '<ButtonConfirm v-bind="args">Click to confirm</ButtonConfirm>'
+  })
 }
-export const WithLabel = Template.bind({})
-WithLabel.args = {
-  class: 'btn btn-info',
-  label: 'Will you bilge Matey?',
-  yes: 'Aye',
-  no: 'Abandon ship!'
+
+export const WithLabel = {
+  args: {
+    class: 'btn btn-info',
+    label: 'Will you bilge Matey?',
+    yes: 'Aye',
+    no: 'Abandon ship!'
+  },
+  render: (args: any) => ({
+    components: { ButtonConfirm },
+    setup: () => ({ args }),
+    template: '<ButtonConfirm v-bind="args">Click to confirm</ButtonConfirm>'
+  })
 }
