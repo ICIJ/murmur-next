ad9a00ddc0260fdb86be3e3723f5e6b2f8d1df43
Pierre Romera <hello@pirhoo.com>
2026-01-05 09:59:05 +0000

    refactor(stories): convert Accordion stories to CSF3 format

--- stories/murmur/components/Accordion/AccordionWrapper.stories.ts
+++ stories/murmur/components/Accordion/AccordionWrapper.stories.ts
@@ -1,5 +1,4 @@
-import { AccordionWrapper } from '@/components'
-import { StoryObj } from '@storybook/vue3-vite'
+import { AccordionWrapper, AccordionStep } from '@/components'
 import { BButton } from 'bootstrap-vue-next'
 import { bgStripedDecorator } from '../../decorators'
 
@@ -10,57 +9,44 @@ export default {
   argTypes: {}
 }
 
-type Story = StoryObj<typeof AccordionWrapper>
 const stepsEnum = Object.freeze({
   MAILVELOPE: Symbol('MAILVELOPE'),
   EXPORT_PUBLIC_KEY: Symbol('EXPORT_PUBLIC_KEY'),
   UPLOAD_PUBLIC_KEY: Symbol('UPLOAD_PGP')
 })
-const data = {
-  step: stepsEnum.MAILVELOPE,
-  steps: Object.values(stepsEnum),
-  active: false
-}
-const Template: Story = (args: any) => ({
-  components: { AccordionWrapper, BButton },
-  setup() {
-    return { stepsEnum, args }
+
+const steps = Object.values(stepsEnum)
+
+export const Default = {
+  args: {
+    steps,
+    step: stepsEnum.MAILVELOPE,
+    class: 'p-2'
   },
-  template: `<AccordionWrapper v-bind="args" @step-change="args.step=$event">
-        <accordion-step
+  decorators: [bgStripedDecorator],
+  render: (args: any) => ({
+    components: { AccordionWrapper, AccordionStep, BButton },
+    setup: () => ({ stepsEnum, args }),
+    template: `
+      <AccordionWrapper v-bind="args" @step-change="args.step = $event">
+        <AccordionStep
           :step="stepsEnum.MAILVELOPE"
           class="accordion-step__mailvelope"
           title="Install Mailvelope"
           content="Install Mailvelope and create your key"
         />
-        <accordion-step
-          :step="stepsEnum.EXPORT_PUBLIC_KEY"
-          title="Export your public key"
-        >
-            <template #content>
-                <p>
-                    Export your public PGP key as a .asc file and download on your
-                    computer
-                </p>
-            </template>
-        </accordion-step>
-        <accordion-step
-          :step="stepsEnum.UPLOAD_PUBLIC_KEY"
-        >
-            <template #title>
-                Upload your publickey
-            </template>
-            <template #nextStepButton>
-                <b-button variant="info">The end!</b-button>
-            </template>
-        </accordion-step>
-    </AccordionWrapper>`
-})
-
-export const Default = Template.bind({})
-Default.decorators = [bgStripedDecorator]
-Default.args = {
-  steps: data.steps,
-  step: data.step,
-  class: 'p-2'
+        <AccordionStep :step="stepsEnum.EXPORT_PUBLIC_KEY" title="Export your public key">
+          <template #content>
+            <p>Export your public PGP key as a .asc file and download on your computer</p>
+          </template>
+        </AccordionStep>
+        <AccordionStep :step="stepsEnum.UPLOAD_PUBLIC_KEY">
+          <template #title>Upload your publickey</template>
+          <template #nextStepButton>
+            <BButton variant="info">The end!</BButton>
+          </template>
+        </AccordionStep>
+      </AccordionWrapper>
+    `
+  })
 }
